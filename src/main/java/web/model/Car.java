6fbf6cf58c8bb705f8c8d59d209f469d63474c55
yeromanika <yeromanika@gmail.com>
2025-05-25 17:51:25 +0300

    Initial commit

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,26 @@
+package web.model;
+
+public class Car {
+    private String model;
+    private String color;
+    private int year;
+
+    public Car(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+}
